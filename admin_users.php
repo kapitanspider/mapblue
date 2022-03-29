0cad1f076e591aed33b8909b842df5a6945f5406
Witold Borowiak <witek.wiry@op.pl>
2022-03-29 16:43:00 +0200

    Dodawanie urzytkownika cz1

--- admin_users.php
+++ admin_users.php
@@ -20,11 +20,19 @@ if(isset($_POST["activate"]))
 	$conn->query($sql);
 }
 
-$sql = "SELECT * From users Where Not ID = '".$_SESSION["USER"]."'";
+$sql = "SELECT * From users Where Not ID = '".$_SESSION["USER"]."' and ADMIN = 0";
 $result = $conn->query($sql);
 ?>
 Użytkownicy
 <table border="solid">
+<tr>
+<th>Login</th>
+<th>Imię</th>
+<th>Nazwisko</th>
+<th>Email</th>
+<th>Telefon</th>
+<th>Zmiana statusu konta</th>
+</tr>
 <?php
 while($row = $result->fetch_assoc())
 {
@@ -68,5 +76,64 @@ echo "</tr>";
 }
 ?>
 </table>
+<?php
+$sql = "SELECT * From users Where Not ID = '".$_SESSION["USER"]."' and ADMIN = 1";
+$result = $conn->query($sql);
+?>
+Administratorzy
+<table border="solid">
+<tr>
+<th>Login</th>
+<th>Imię</th>
+<th>Nazwisko</th>
+<th>Email</th>
+<th>Telefon</th>
+<th>Zmiana statusu konta</th>
+</tr>
+<?php
+while($row = $result->fetch_assoc())
+{
+//var_dump($row);
+if($row["IS_ACTIVE"]==1)
+{
+echo "<tr bgcolor=lime>";
+}
+else
+{
+echo "<tr bgcolor=red>";	
+}
+echo "<td>".$row["LOGIN"]."</td>";
+echo "<td>".$row["IMIE"]."</td>";
+echo "<td>".$row["NAZWISKO"]."</td>";
+echo "<td>".$row["EMAIL"]."</td>";
+echo "<td>".$row["TELEFON"]."</td>";
+if($row["IS_ACTIVE"]==1)
+{
+?>
+<td>
+<form action="admin_users.php" method="post">
+<input type="hidden" name="deactivate" value=<?php echo  $row["ID"]; ?>>
+<input type="submit" value="Dezaktywuj">
+</form>
+</td>
+<?php
+}
+else
+{
+?>
+<td>
+<form action="admin_users.php" method="post">
+<input type="hidden" name="activate" value=<?php echo  $row["ID"]; ?>>
+<input type="submit" value="Aktywuj">
+</form>
+</td>
+<?php	
+}
+echo "</tr>";
+}
+?>
+</table>
+<br>
+<a href="admin_users_add_new.php">Dodaj urzytkownika</a>
 </body>
 </html>
---
+++ admin_users_add_new.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Title of the document</title>
+
+</head>
+<body>
+<?php
+include('facecheck.php');
+include('dbconfig.php');
+
+?>
+<a href="test/test.php">XD</a>
+</body>
+</html>
