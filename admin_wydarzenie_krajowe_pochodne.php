036ffde3ac1df8f97fe5c5ba9f3bc36e6b8aa75c
Witold Borowiak <witek.wiry@op.pl>
2022-04-26 19:38:20 +0200

    Rozpoczęcie integracji bootstrapa

---
+++ admin_wydarzenie_krajowe_pochodne.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Title of the document</title>
+<script
+src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
+</script>
+</head>
+<body>
+<?php
+include('facecheck.php');
+include('dbconfig.php');
+var_dump($_POST);
+$sql= "SELECT aktywnosci.ID, users.IMIE,users.NAZWISKO , aktywnosci.ID_Organizatora, aktywnosci.wojewodztwo, aktywnosci.okreg, aktywnosci.powiat, aktywnosci.gmina, aktywnosci.nazwa, aktywnosci.rodzaj, aktywnosci.data, aktywnosci.godzina, aktywnosci.uczestnicy, aktywnosci.potwierdzenie, aktywnosci.notatka, aktywnosci.data_dodania, aktywnosci.ocena FROM aktywnosci INNER JOIN users ON aktywnosci.ID_Organizatora=users.ID Where ID_Parent='".$_POST["id"]."' order by users.IMIE, users.NAZWISKO asc";
+$result = $conn->query($sql);
+echo "<table border='solid'>";
+
+while($row = $result->fetch_assoc())
+{
+echo "<tr>";
+echo "<td>".$row["IMIE"]." ".$row["NAZWISKO"]."</td>";
+echo "<td>".$row["wojewodztwo"]." / ".$row["okreg"]." / ".$row["powiat"]." / ".$row["gmina"]."</td>";
+echo "<td>".$row["nazwa"]."</td>";
+echo "<td>".$row["data"]."</td>";
+echo "<td>".$row["godzina"]."</td>";
+echo "<td>".$row["uczestnicy"]."</td>";
+echo "<td><a href='".$row["potwierdzenie"]."' target='blank'>"."Link"."</a></td>";
+echo "<td>".$row["data_dodania"]."</td>";
+echo "<td>".$row["ocena"]."</td>";
+echo "</tr>";
+}
+echo "</table>";
+?>
+<br>
+<a href="main.php">Wróć</a>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,27 +1,47 @@
 <!DOCTYPE HTML>
-<html>
+<html lang="PL">
 <head>
-<title>Title of the document</title>
+<title>Map Blue</title>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
-<form action="login.php" method="post">
-Login: <input type="text" name="Login"><br>
-Hasło: <input type="password" name="Password"><br>
-<input type="submit">
-</form>
+<br>
+
+<div class="d-flex justify-content-center m-1">
+	<form action="login.php" method="post" class="card p-2 w-100">
+	<h2 class="text-center m-3 text-primary">MapBlue</h2> 
+	<img src="images/Poland.svg" class="rounded mx-auto d-block img-thumbnail" style="max-width: 250px;" alt="...">
+	
 <?php
 include('dbconfig.php');
 if(isset($_GET['login']))
 {
 	if($_GET['login']=="error")
 	{
-	echo "błędny login lub hasło";
+	echo '<script>alert("Błędny login lub hasło");</script>';
 	}
 	if($_GET['login']=="notactive")
 	{
-	echo "konto zostało zablokowane przez administratora";
+	echo '<script>alert("Konto zostało wyłączone przez administratora");</script>';
 	}
 }
 ?>
+	
+	<div class="container text-center ">
+	<p style="font-size:xx-large;">Login:</p> 
+	<input class="form-control text-center w-75 mx-auto d-block" type="text" name="Login" style="font-size:x-large;"><br>
+	<p style="font-size:xx-large;">Hasło:</p> 
+	<input class="form-control text-center w-75 mx-auto d-block" type="password" name="Password" style="font-size:x-large;"><br>
+	<input type="submit" class="btn btn-primary">
+	</div>
+	</form>
+</div>
+
+<footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">&copy;2022 Company Name</p>
+  </footer>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 </html>
