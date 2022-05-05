63e89605dbf632ee8819f4408dd36f16209008af
Witold Borowiak <witek.wiry@op.pl>
2022-05-05 17:48:47 +0200

    Boostrap cd

--- profil.php
+++ profil.php
@@ -1,8 +1,8 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Title of the document</title>
-</head>
+<title>Map Blue - Profil</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></head>
 <body>
 <?php
 include('facecheck.php');
@@ -15,22 +15,58 @@ while($row = $result->fetch_assoc()){
 			$user = $row;
 		}
 ?>
-<p>Profil</p>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand" href="main.php"><span class="h3">MapBlue</span></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="main.php">Strona główna</a>
+      </li>
+	  <li class="nav-item active">
+        <a class="nav-link" href="profil.php">Profil</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="map.php">Dodaj aktywność</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="kalendarz_user.php">Kalendarz</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="ustawienia.php">Ustawienia</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="pomoc.php">Pomoc</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="wydarzenia_krajowe.php">Wydarzenia Ogólnopolskie</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="user_udostepnione.php">Udostępnione</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br>
+<div class="container-fluid p-2 card" style="max-width:500px;">
 <?php
-echo '<img src="'.$user["Profilowe"].'" width="200px" height="200px">';
-echo '<p>'.$user["IMIE"]." ".$user["NAZWISKO"].'</p>';
-echo '<p>Login: '.$user["LOGIN"].'</p>';
-echo '<p>Nr. Okręgu: '.$user["NR_OKREGU"].'</p>';
-echo '<p>Specjalizacja: '.$user["SPECJALIZACJA"].'</p>';
-echo '<p>Funkcja: '.$user["FUNKCJA"].'</p>';
-echo '<p>Adres email: '.$user["EMAIL"].' <a href="change_email.php">Zmień</a></p>';
-echo '<p>Nr. telefonu: '.$user["TELEFON"].' <a href="change_tel.php">Zmień</a></p>';
+echo '<img class="card-img-top" src="'.$user["Profilowe"].'">';
+echo '<div class="card-body"><h5 class="card-title">'.$user["IMIE"]." ".$user["NAZWISKO"].'</h5>';
+echo '<p class="card-text">Login: '.$user["LOGIN"].'</p>';
+echo '<p class="card-text">Nr. Okręgu: '.$user["NR_OKREGU"].'</p>';
+echo '<p class="card-text">Specjalizacja: '.$user["SPECJALIZACJA"].'</p>';
+echo '<p class="card-text">Funkcja: '.$user["FUNKCJA"].'</p>';
+echo '<p class="card-text">Adres email: '.$user["EMAIL"].' </p>';
+echo '<p class="card-text">Nr. telefonu: '.$user["TELEFON"].' </p>';
+echo '<a class="btn btn-primary m-1"  href="change_email.php">Zmień email</a>';
+echo '<a class="btn btn-primary m-1" href="change_pass.php">Zmień hasło</a>';
+echo '<a class="btn btn-primary m-1" href="change_tel.php">Zmień nr. Telefonu</a> ';
+echo '</div>';
 ?>
-<br>
-<br>
-<br>
-<a href="change_pass.php">Zmień hasło</a>
-<br>
-<a href="main.php">Ekran główny</a>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
